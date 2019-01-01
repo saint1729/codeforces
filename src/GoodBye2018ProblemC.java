9404af3713c0cd3e6337f8dbf78ce1f064459e4f
Sai Nikhil <tsnlegend@gmail.com>
2019-01-01 23:55:32 +0530

    Added "Good Bye 2018 - Problem C" solution

--- src/GoodBye2018ProblemC.java
+++ src/GoodBye2018ProblemC.java
@@ -1,47 +1,66 @@
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeSet;
 
 public class GoodBye2018ProblemC {
 
-
-    public static int fun(int n, int k) {
-        int sum = 0;
-        int x = 1;
-
-        do {
-            sum += (x == 0 ? n : x);
-            x += k;
-            x = x%n;
-        } while(x != 1);
-
-        return sum;
-
-    }
-
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt(), k = 1;
-
-        Set<Integer> s = new TreeSet<>();
-
-        while(k <= n) {
-            s.add(fun(n , k));
-            // System.out.println(n + " " + k + " " + fun(n, k));
-            k += 1;
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(Integer i : s) {
-            sb.append(i + " ");
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
+     public static void main(String args[]) {
+           Scanner sc = new Scanner(System.in);
+           long n = sc.nextInt();
+           Stack<Long> s = new Stack();
+           for(long i=1; i*i <= n; i++) {
+               if(n%i == 0) {
+                   System.out.print(n*(i-1)/2+i+" ");
+                   if (i*i < n) {
+                       long j = n/i;
+                       s.add(n*(j-1)/2+j);
+                   }
+               }
+           }
+           while(!s.isEmpty()) {
+               long j = s.pop();
+               System.out.print(j+" ");
+           }
+     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
