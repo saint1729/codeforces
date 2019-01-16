629d22c1672361ab1c2255f59e603d80a3084f42
Sai Nikhil <tsnlegend@gmail.com>
2019-01-16 23:21:35 +0530

    Codeforces practice

--- output/Main.java
+++ output/Main.java
@@ -31,7 +31,17 @@ public class Main {
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int n = in.nI(), k = in.nI();
             int[] a = in.nextIntArray(n);
-            out.p(-30 / 47);
+            //out.p(-30/47);
+
+            for (int b = 1; b <= n; b++) {
+                int count = 0;
+                for (int i = -n / 2; i <= (n - 1) / 2; i++) {
+                    int c = b + i * k;
+                    if (c > 0) {
+                        count++;
+                    }
+                }
+            }
         }
 
     }
@@ -49,10 +59,6 @@ public class Main {
 
         public void close() {
             writer.close();
-        }
-
-        public void p(int i) {
-            writer.print(i);
         }
 
     }
